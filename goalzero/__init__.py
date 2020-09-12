e3a9f3108917f086dbbe4ff5ab26ce43bdc59407
@tkdrob <tkdrob4390@yahoo.com>
2020-09-12 16:43:25 -0400

    ignore ServerDisconnectedError

--- goalzero/__init__.py
+++ goalzero/__init__.py
@@ -1,13 +1,13 @@
-"""
-Goal Zero Rest Api
-~~~~~~~~~~~~~~~~~~~~~
+"""Goal Zero Rest Api."""
 
-:copyright: (c) 2020 by Robert Hillis.
-Not affiliated with Goal Zero. Credit for headers belongs to Goal Zero.
-:license: Apache 2.0, see LICENSE for more details.
-"""
 import aiohttp
+from async_timeout import timeout
 from . import exceptions
+
+""":copyright: (c) 2020 by Robert Hillis.
+Not affiliated with Goal Zero. Credit for headers belongs to Goal Zero.
+:license: Apache 2.0, see LICENSE for more details."""
+
 
 HEADER = {
     "Content-Type": "application/json",
@@ -22,161 +22,117 @@ HEADER = {
 TIMEOUT = 10
 
 
-class GoalZero:
+class Yeti:
     """A class for handling connections with a Goal Zero Yeti."""
 
     def __init__(self, host, loop, session, **data):
         """Initialize the connection to Goal Zero Yeti instance."""
         self._loop = loop
         self._session = session
-        self.host = host
+        self.control = {}
         self.data = {}
+        self.endpoint = {}
+        self.host = host
+        self.payload = {}
         for key, value in data.items():
             if key == "payload":
                 self.payload = value
 
     async def get_state(self):
         """Get states from Goal Zero Yeti instance."""
-
-        try:
-            async with self._session.get(
-                "http://" + self.host + "/state", timeout=TIMEOUT
-            ) as response:
-
-                self.data = await response.json()
-                if "thingName" in self.data:
-                    return self.data
-                else:
-                    raise exceptions.InvalidHost()
-        except aiohttp.client_exceptions.ClientConnectorError:
-            raise exceptions.ConnectError() from aiohttp.client_exceptions.ClientConnectorError
+        self.control = "thingName"
+        self.endpoint = "/state"
+        await Yeti.send_get(self)
 
     async def post_state(self):
         """Post payload to Goal Zero Yeti instance."""
-        async with self._session.post(
-            "http://" + self.host + "/state",
-            json=self.payload,
-            headers=HEADER,
-            verify=False,
-            timeout=TIMEOUT,
-        ) as request:
-            self.data = await request.json()
-        return self.data
+        self.endpoint = "/state"
+        await Yeti.send_post(self)
 
     async def sysinfo(self):
         """Get system info from Goal Zero Yeti instance."""
-        endpoint = "/sysinfo"
-        async with self._session.get(
-            "http://" + self.host + endpoint, timeout=TIMEOUT
-        ) as response:
-            self.data = await response.json()
-        return self.data
+        self.endpoint = "/sysinfo"
+        self.control = "name"
+        await Yeti.send_get(self)
 
     async def reset(self):
         """Reset Goal Zero Yeti instance."""
-        self.payload = {}
-        endpoint = "/factory-reset"
-        async with self._session.post(
-            "http://" + self.host + endpoint,
-            json=self.payload,
-            headers=HEADER,
-            verify=False,
-            timeout=TIMEOUT,
-        ) as request:
-            self.data = await request.json()
-        return self.data
+        self.endpoint = "/factory-reset"
+        await Yeti.send_get(self)
 
     async def reboot(self):
         """Reboot Goal Zero Yeti instance."""
-        endpoint = "/rpc/Sys.Reboot"
-        async with self._session.get(
-            "http://" + self.host + endpoint, timeout=TIMEOUT
-        ) as response:
-            self.data = await response.json()
-        return self.data
+        self.endpoint = "/rpc/Sys.Reboot"
+        await Yeti.send_get(self)
 
     async def get_loglevel(self):
         """Get log level Goal Zero Yeti instance."""
-        endpoint = "/loglevel"
-        async with self._session.get(
-            "http://" + self.host + endpoint, timeout=TIMEOUT
-        ) as response:
-            self.data = await response.json()
-        return self.data
+        self.endpoint = "/loglevel"
+        self.control = "app"
+        await Yeti.send_get(self)
 
     async def post_loglevel(self):
         """Post log level Goal Zero Yeti instance."""
-        endpoint = "/loglevel"
-        async with self._session.post(
-            "http://" + self.host + endpoint,
-            json=self.payload,
-            headers=HEADER,
-            verify=False,
-            timeout=TIMEOUT,
-        ) as request:
-            self.data = await request.json()
-        return self.data
+        self.endpoint = "/loglevel"
+        await Yeti.send_post(self)
 
     async def join(self):
         """Join Goal Zero Yeti instance to Wifi network."""
-        endpoint = "/join"
-        async with self._session.post(
-            "http://" + self.host + endpoint,
-            json=self.payload,
-            headers=HEADER,
-            verify=False,
-            timeout=TIMEOUT,
-        ) as request:
-            self.data = await request.json()
-        return self.data
+        self.endpoint = "/join"
+        await Yeti.send_post(self)
 
     async def wifi(self):
         """Get available Wifi networks visible to Goal Zero Yeti instance."""
-        endpoint = "/wifi"
-        async with self._session.get(
-            "http://" + self.host + endpoint, timeout=TIMEOUT
-        ) as response:
-            self.data = await response.json()
-        return self.data
+        self.endpoint = "/wifi"
+        await Yeti.send_get(self)
 
     async def password_set(self):
         """Set password to Zero Yeti instance."""
         self.payload = {"new_password": "" + self.payload + ""}
-        endpoint = "/password-set"
-        async with self._session.post(
-            "http://" + self.host + endpoint,
-            json=self.payload,
-            headers=HEADER,
-            verify=False,
-            timeout=TIMEOUT,
-        ) as request:
-            self.data = await request.json()
-        return self.data
+        self.endpoint = "/password-set"
+        await Yeti.send_post(self)
 
     async def join_direct(self):
         """Directly Join Goal Zero Yeti instance on its Wifi."""
-        self.payload = {}
-        endpoint = "/join-direct"
-        async with self._session.post(
-            "http://" + self.host + endpoint,
-            json=self.payload,
-            headers=HEADER,
-            verify=False,
-            timeout=TIMEOUT,
-        ) as request:
-            self.data = await request.json()
-        return self.data
+        self.endpoint = "/join-direct"
+        await Yeti.send_post(self)
 
     async def start_pair(self):
         """Pair with Goal Zero Yeti instance."""
-        self.payload = {}
-        endpoint = "/start-pair"
-        async with self._session.post(
-            "http://" + self.host + endpoint,
-            json=self.payload,
-            headers=HEADER,
-            verify=False,
-            timeout=TIMEOUT,
-        ) as request:
-            self.data = await request.json()
+        self.endpoint = "/start-pair"
+        await Yeti.send_post(self)
+
+    async def send_get(self):
+        """Send get request."""
+        try:
+            async with timeout(TIMEOUT, loop=self._loop):
+                response = await self._session.get(
+                    "http://" + self.host + self.endpoint
+                )
+                self.data = await response.json()
+                if self.control not in self.data:
+                    raise exceptions.InvalidHost()
+        except aiohttp.client_exceptions.ClientConnectorError:
+            raise exceptions.ConnectError() from aiohttp.client_exceptions.ClientConnectorError
+        except aiohttp.client_exceptions.ServerDisconnectedError:
+            pass
+
+    async def send_post(self):
+        """Send post request."""
+        try:
+            async with timeout(TIMEOUT, loop=self._loop):
+                request = self._session.post(
+                    "http://" + self.host + self.endpoint,
+                    json=self.payload,
+                    headers=HEADER,
+                    verify=False,
+                )
+                self.data = await request.json()
+        except aiohttp.client_exceptions.ClientConnectorError:
+            raise exceptions.ConnectError() from aiohttp.client_exceptions.ClientConnectorError
+        except aiohttp.client_exceptions.ServerDisconnectedError:
+            pass
+
+    def status(self):
+        """Return the status of the Yeti."""
         return self.data
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="goalzero",
-    version="0.1.1",
+    version="0.1.2",
     author="Robert Hillis",
     author_email="tkdrob4390@yahoo.com",
     description="Goal Zero REST Api Library",
